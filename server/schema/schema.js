2c6debb257b6d7378b6b5cca9b9ce9df7fc5340e
Adam Monroe <yumangkara@gmail.com>
2020-03-23 20:32:05 +0800

    Updates on server project
    * added sorting on mongodb fetching

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -15,29 +15,6 @@ const
     GraphQLNonNull
 } = graphql;
 
-//#region dummy data
-// dummy data
-// var songs = [
-//     {id: "1", title: "Alimango", genre: "Rock", artist_id: '1', album: "Alimango Salad"},
-//     {id: "2", title: "Black Sheep", genre: "Rock", artist_id: '1', album: "Alimango Salad"},
-//     {id: "3", title: "Maalaala mo kaya", genre: "Rock", artist_id: '2', album: "Release"},
-//     {id: "4", title: "Permanent Change", genre: "Alternative Rock", artist_id: '1', album: "Alimango Salad"},
-//     {id: "5", title: "Osige", genre: "Alternative Rock", artist_id: '1', album: "Alimango Salad"},
-//     {id: "6", title: "Kaibigan", genre: "Alternative Rock", artist_id: '1', album: "Alimango Salad"},
-//     {id: "7", title: "Kamusta", genre: "Alternative Rock", artist_id: '2', album: "Release"},
-//     {id: "8", title: "Pagsisisi", genre: "Mellow", artist_id: '2', album: "Release"},
-//     {id: "9", title: "Shut up", genre: "Alternative", artist_id: '3', album: "HighSchool"},
-//     {id: "10", title: "Purple's so sad so lonely he's gonna cry", genre: "Dark Mellow", artist_id: '4', album: "HighSchool Comps"},
-// ];
-
-// var artists = [
-//     { id: "1", name: "Long Bond Yellow Papers", year_started: 2008},
-//     { id: "2", name: "LBYP", year_started: 2008},
-//     { id: "3", name: "The Classmates", year_started: 2005},
-//     { id: "4", name: "The Flowers", year_started: 2005},
-// ];
-//#endregion
-
 const SongType = new GraphQLObjectType({
     name: 'Song',
     fields: () => ({
@@ -64,8 +41,7 @@ const ArtistType = new GraphQLObjectType({
         songs: {
             type: new GraphQLList(SongType),
             resolve(parent, args){
-                return Song.find({ artist_id: parent.id });
-                //return _.filter(songs, {artist_id: parent.id})
+                return Song.find({ artist_id: parent.id }).sort({ title: 1 });
             }
         }
     })
@@ -79,7 +55,6 @@ const RootQuery = new GraphQLObjectType({
             args: { id: { type: GraphQLID }},
             resolve(parent, args){
                 return Song.findById(args.id);
-              //return _.find(songs, { id: args.id });
             }
         },
         artist: {
@@ -87,21 +62,18 @@ const RootQuery = new GraphQLObjectType({
             args: { id: { type: GraphQLID }},
             resolve(parent, args){
                 return Artist.findById(args.id);
-                //return _.find(artists, { id: args.id });
             }
         },
         songs: {
             type: new GraphQLList(SongType),
             resolve() {
-                return Song.find({});
-                //return songs;
+                return Song.find({}).sort({ title: 1 });
             }
         },
         artists: {
             type: new GraphQLList(ArtistType),
             resolve() {
-                return Artist.find({});
-                //return artists;
+                return Artist.find({}).sort({ name: 1 });
             }
         }
     }
